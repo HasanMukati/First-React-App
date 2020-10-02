4f4fa2753a0926a3f4423c3422ea2ea491be849b
Hasan Mukati <mukati.hasan@gmail.com>
2020-10-01 22:30:51 -0400

    First Commit
    React To-do with a little CSS

---
+++ src/components/sylesheet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+function Stylesheet(){
+    return(
+        <div>
+            <h1> Made by Hasan</h1>
+        </div>
+
+    )
+
+}
+
+export default Stylesheet
